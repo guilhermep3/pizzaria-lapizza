8c96cfadd37003e85822fbdb83b7a4889983d0d5
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2024-11-23 16:01:01 -0300

    adding payments

---
+++ src/js/payments.js
@@ -0,0 +1,18 @@
+const payments = [
+   {
+      name: 'Elo',
+      img: 'elo.png'
+   },
+   {
+      name: 'Mastercard',
+      img: 'mastercard.png'
+   },
+   {
+      name: 'Pix',
+      img: 'pix.png'
+   },
+   {
+      name: 'Visa',
+      img: 'visa.png'
+   },
+]
